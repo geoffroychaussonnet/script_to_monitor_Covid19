3314309f3943d90166c11b4fd922f22376d1facc
Geoffroy <geoffroy.chaussonnet@kit.edu>
2020-03-22 08:26:07 +0100

    last minor modif.

--- plotCorrelationDeathsQuarantine.py
+++ plotCorrelationDeathsQuarantine.py
@@ -12,6 +12,8 @@ path="../csse_covid_19_data/csse_covid_19_time_series/"
 daysInterval = 7   
 # Start date of the plot:
 startDate = datetime.date(2020, 2,22)   
+extrapolPeriod = 14     # How many days to extrapolate?
+fittingPeriod = 8       # On how long do we fit the data?
 
 # basic use:
 # example: plot_country("France",filterDate,dates,'3/17/20',ax)
@@ -31,23 +33,25 @@ def evolution_country(strCountry,filterDate):
         evolution[:] += data.iloc[ic].values[4:].astype(int)
     return evolution[filterDate]
 
-def get_trend(dates,evol1,startMonth,startDay,deltaDays):
+
+def get_trend(dates,evol1,startMonth,startDay,fitParam):
+    fittingPeriod = fitParam[0]
+    extrapolPeriod = fitParam[1]
     startDate = datetime.date(2020, startMonth,startDay)
-    endDate = startDate + dt.timedelta(days=9)
+    endDate = startDate + dt.timedelta(days=fittingPeriod+1)
     bcorrelDate = (dates>=startDate) * (dates<=endDate)
     correlDate = dates[bcorrelDate]
     x = np.arange(sum(bcorrelDate))
     y = evol1[bcorrelDate]
     p1=polyfit(x,log(y),1)
     print(p1)
-    #xpredict = np.arange(sum(bcorrelDate)+deltaDays)
-    xpredict = np.arange(sum(bcorrelDate)-1,sum(bcorrelDate)+deltaDays)
+    xpredict = np.arange(sum(bcorrelDate)-1,sum(bcorrelDate)+extrapolPeriod)
     pol = exp(polyval(p1,xpredict))
     correl1 = pol
     xcorrel1 = dateax[bcorrelDate].tolist()
     xcorrel1 = [dateax[bcorrelDate][-1]]
     correlDate = correlDate.tolist()
-    for i in range(deltaDays):
+    for i in range(extrapolPeriod):
         correlDate.append(correlDate[-1] + dt.timedelta(days=1))
         xcorrel1.append(dateOut(correlDate[-1]))
 
@@ -62,8 +66,10 @@ def dateIn(strDate):
     year = int("20%s" %spl[2])
     return datetime.date(year, month,day)
 
-def plot_country(strCountry,filterDate,dates,quarParam,ax):
+def plot_country(strCountry,filterDate,dates,fitParam,quarParam,ax):
     quarDate = quarParam
+    fittingPeriod = fitParam[0]
+    extrapolPeriod = fitParam[1]
 
     # Extract evolution for this country
     evol1 = evolution_country(strCountry,filterDate)
@@ -74,19 +80,31 @@ def plot_country(strCountry,filterDate,dates,quarParam,ax):
     # Define the period for the trend
     if sum(iQuar) > 0: # Quarantine found
         quarDateIn = dateIn(quarDate)
+
+        correlDateStart2 = dateIn(quarDate)
+        correlDateEnd2 = dt.date.today()
+        fittingPeriod2 = (correlDateEnd2 - correlDateStart2).days + 1
+        startMonth2 = correlDateStart2.month
+        startDay2 = correlDateStart2.day
+        fitParam2 = [fittingPeriod2, extrapolPeriod]
     else:
         quarDateIn = dt.date.today()
-    correlDateStart = quarDateIn - dt.timedelta(days=9)
+    correlDateStart = quarDateIn - dt.timedelta(days=fittingPeriod+1)
     print("Start correl date before quanratine (or today) :", dateOut(correlDateStart))
     startMonth = correlDateStart.month
     startDay = correlDateStart.day
-    deltaDays = 14
 
-    # Get the trend
-    xcorrel1, correl1 = get_trend(dates,evol1,startMonth,startDay,deltaDays)
     p = ax.semilogy(dateax,evol1,ls='-',lw=4.0,label=strCountry)
     col = p[0].get_color()
+
+    # Get the trend
+    xcorrel1, correl1 = get_trend(dates,evol1,startMonth,startDay,fitParam)
     ax.semilogy(xcorrel1,correl1,ls='-',lw=2.0,c=col)
+    
+    if sum(iQuar) > 0: # Quarantine found
+        xcorrel2, correl2 = get_trend(dates,evol1,startMonth2,startDay2,fitParam2)
+        ax.semilogy(xcorrel2,correl2,ls='-',lw=2.0,c=col)
+
     # Plot the quarantine date
     ax.scatter(dateax[iQuar[0]],evol1[iQuar[0]],c=col,s=300,marker="X")
 
@@ -98,6 +116,8 @@ Ndays = len(dateax)
 # Convert date axis to date vector
 dates = np.array([dt.datetime.strptime(plof,'%m/%d/%y').date() for plof in dateax])
 
+fitParam = [fittingPeriod, extrapolPeriod]
+
 # Filter axe of dates
 filterDate = (dates>=startDate)
 dateax = dateax[filterDate]
@@ -107,13 +127,13 @@ close(1)
 fig = figure(1)
 ax = fig.add_subplot(111)
 
-plot_country("France",filterDate,dates,'3/17/20',ax)
-plot_country("Germany",filterDate,dates,'3/19/20',ax)
-plot_country("Italy",filterDate,dates,'3/9/20',ax)
-plot_country("Spain",filterDate,dates,'3/14/20',ax)
-plot_country("United Kingdom",filterDate,dates,'3/22/20',ax)
-plot_country("US",filterDate,dates,'3/22/20',ax)
-#plot_country("Korea, South",filterDate,dates,'3/22/20',ax)
+plot_country("France",filterDate,dates,fitParam,'3/17/20',ax)
+plot_country("Germany",filterDate,dates,fitParam,'3/19/20',ax)
+plot_country("Italy",filterDate,dates,fitParam,'3/9/20',ax)
+plot_country("Spain",filterDate,dates,fitParam,'3/14/20',ax)
+plot_country("United Kingdom",filterDate,dates,fitParam,'3/22/20',ax)
+plot_country("US",filterDate,dates,fitParam,'3/22/20',ax)
+#plot_country("Iran",filterDate,dates,'3/22/20',ax)
 
 ax.set_title("Cumulated deaths in some Western countries\n (Source: Johns Hopkins University)")
 
@@ -127,6 +147,6 @@ ax.grid(which='major',color='grey', linestyle='-', linewidth=1)
 ax.grid(which='minor',color='grey', linestyle='-', linewidth=0.5)
 
 strDateToday = dt.date.today().strftime("%Y%m%d")
-fname = "../evolCovid19_Europe_Death_Prediction%idays_stand%s.pdf" %(deltaDays,strDateToday)
+fname = "../evolCovid19_Europe_Death_Prediction%idays_stand%s.pdf" %(14,strDateToday)
 savefig(fname)
 show()
